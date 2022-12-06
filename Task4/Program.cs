b1bd8f00a2e581500cf9d7eea42cb442e51c4c25
Sanzhar <dos.0227@yandex.ru>
2022-12-06 13:49:27 +0600

    Task4 function update

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -3,24 +3,34 @@
 // 82 -> 2
 // 9,012 -> 4
 
-Console.WriteLine($"Введите число");
-
-double num = Convert.ToDouble(Console.ReadLine());
-double temp = num;
-if (temp < 0) temp = -temp;
-int count = 0;
-if (temp < 1) count = 1;
-while (temp % 1 / 10 != 0)
+double Counting(double num)
 {
-    temp = temp * 10;
+    double temp = num;
+    if (temp < 0) temp = -temp;
+    int count = 0;
+    if (temp < 1) count = 1;
+    while (temp % 1 / 10 != 0)
+    {
+        temp = temp * 10;
+    }
+    int num1 = Convert.ToInt32(temp);
+    while (num1 != 0)
+    {
+        num1 = num1 / 10;
+        count++;
+    }
+    return count;
 }
 
-int num1 = Convert.ToInt32(temp);
-
-while (num1 != 0)
+try
 {
-    num1 = num1 / 10;
-    count++;
+    Console.WriteLine($"Введите число");
+    double num = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Количество цифр в числе {num} = {Counting(num)}");
 }
 
-Console.WriteLine($"Количество цифр в числе {num} = {count}");
+catch
+{
+    Console.WriteLine("Надо ввести целое или дробное число!!!");
+}
+
